16ec10896bd87197fa0c5821034864bd1a8664a1
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2023-11-25 17:34:01 +0300

    Add files via upload

--- UDPTesla.sh
+++ UDPTesla.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-sudo apt-get install jq -y
+sudo apt-get install jq 
 set -e
 # [Add a custom server banner to Welcome]
 wget https://raw.githubusercontent.com/TeslaSSH/X-teria/main/hysteria -O ~/udp/hystban.sh &>/dev/null
